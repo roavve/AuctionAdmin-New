fd81332177ba07bef46c4b8aca23caf7dcbbad48
warskwlavi <ghirahimdebbie@gmail.com>
2026-05-08 12:42:28 +0400

    Initial commit

---
+++ auction-admin/src/main/java/epg/auction/admin/AuctionAdminApplication.java
@@ -0,0 +1,11 @@
+package epg.auction.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionAdminApplication.class, args);
+    }
+}
