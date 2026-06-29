c65b165bbfbd78d70ad23a953803f0b98c5193e1
Sutidze <marihsutidze@gmail.com>
2026-06-29 16:41:58 +0400

    b4 replace

--- auction-admin/src/main/java/epg/auction/admin/config/CorsConfig.java
+++ auction-admin/src/main/java/epg/auction/admin/config/CorsConfig.java
@@ -6,22 +6,28 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
 
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedOrigin("http://localhost:3001");
-        config.addAllowedOrigin("http://localhost:4200");
+
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:4200",
+                "http://10.1.1.171:4200"
+        ));
 
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
+
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
+
         return new CorsFilter(source);
     }
 }
